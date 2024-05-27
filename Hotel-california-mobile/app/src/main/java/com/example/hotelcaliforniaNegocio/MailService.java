607d52140d1c6af029ecf082cc0093eb9815dcbe
Federico Silva <fedes7777@gmail.com>
2024-05-27 16:03:38 -0300

    chg: se borra clase sin usar

--- Hotel-california-mobile/app/src/main/java/com/example/hotelcaliforniaNegocio/MailService.java
+++
@@ -1,6 +0,0 @@
-package com.example.hotelcaliforniaNegocio;
-
-public class MailService {
-    private static final String mailUsername = "hsoporte.hotelcalifornia@gmail.com";
-    private static final String mailPassword = "qrph wton uhvt avjr";
-}
